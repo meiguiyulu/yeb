d38becc35e8d22f1689ce85ff9c0512e2edf9683
玫瑰与鹿 <33453157+meiguiyulu@users.noreply.github.com>
2022-04-20 15:49:45 +0800

    更新、删除员工

--- yeb-back/server/src/main/java/com/lyj/server/controller/EmployeeController.java
+++ yeb-back/server/src/main/java/com/lyj/server/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import com.lyj.server.pojo.*;
 import com.lyj.server.service.*;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -87,7 +88,7 @@ public class EmployeeController {
 
     @ApiOperation(value = "获取工号")
     @GetMapping("/maxWorkID")
-    public RespBean maxWorkID(){
+    public RespBean maxWorkID() {
         return employeeService.maxWorkId();
     }
 
@@ -95,6 +96,24 @@ public class EmployeeController {
     @PostMapping("/")
     public RespBean addEmp(@RequestBody Employee employee) {
         return employeeService.addEmp(employee);
+    }
+
+    @ApiOperation(value = "更新员工")
+    @PutMapping("/")
+    public RespBean updateEmp(@RequestBody Employee employee) {
+        if (employeeService.updateById(employee)) {
+            return RespBean.success("更新成功");
+        }
+        return RespBean.error("更新失败");
+    }
+
+    @ApiOperation(value = "删除员工")
+    @DeleteMapping("/{id}")
+    public RespBean deleteEmp(@PathVariable Integer id) {
+        if (employeeService.removeById(id)) {
+            return RespBean.success("删除成功");
+        }
+        return RespBean.error("删除失败");
     }
 
 }
